1fdf4bfeddc0b3fd281dcadbe3b7eed59fd4c055
abdelkader <gousmi.abdelkader@gmail.com>
2021-10-14 10:56:16 +0100

    create recipe form and description collumn in recipe table

--- database/migrations/2021_10_13_105320_create_recipes_table.php
+++ database/migrations/2021_10_13_105320_create_recipes_table.php
@@ -17,6 +17,7 @@ class CreateRecipesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('category');
+            $table->text('description');
             $table->timestamps();
         });
     }
--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -2,45 +2,50 @@
 @section('title', 'Create new recipe')
     
 @section('content')
-<div class="card card-primary">
-    <div class="card-header">
-      <h1 class="card-title">Fill the form to create a new recipe:</h1>
-    </div>
-    <!-- /.card-header -->
-    <!-- form start -->
-    <form role="form">
-      <div class="card-body">
-        <div class="form-group">
-          <label for="name">Name:</label>
-          <input type="email" class="form-control" id="name" placeholder="Enter email">
+    <div class="card card">
+        <div class="card-header">
+        <h1 class="card-title">Fill the form to create a new recipe:</h1>
         </div>
-        <div class="form-group">
-            <label>Category</label>
-            <select class="form-control select2bs4" style="width: 100%;">
-                <option selected="selected">Starter</option>
-                <option>Main course</option>
-                <option>Dessert</option>
-              </select></div>
-        <div class="form-group">
-          <label for="exampleInputFile">File input</label>
-          <div class="input-group">
-            <div class="custom-file">
-              <input type="file" class="custom-file-input" id="exampleInputFile">
-              <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form role="form">
+        <div class="card-body">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="email" class="form-control" id="name" placeholder="Enter name">
             </div>
-            <div class="input-group-append">
-              <span class="input-group-text" id="">Upload</span>
+            <div class="form-group">
+                <label>Category</label>
+                <select class="form-control select2bs4" style="width: 100%;">
+                    <option selected="selected">Starter</option>
+                    <option>Main course</option>
+                    <option>Dessert</option>
+                </select>
             </div>
-          </div>
+            <div class="form-group">
+                <label>Description</label>
+                <textarea class="form-control" rows="3" placeholder="Enter the description here"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                    </div>
+                    <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <!-- /.card-body -->
+        <!-- /.card-body -->
 
-      <div class="card-footer">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
-    </form>
-  </div>
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        </form>
+    </div>
 
 @endsection
 
