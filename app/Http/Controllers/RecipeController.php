3c7fc2cf5207b9b6355f71ff0b2b294c70f75b7b
abdelkader <gousmi.abdelkader@gmail.com>
2021-10-14 18:55:40 +0100

    recipe controller fix show

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -73,9 +73,9 @@ class RecipeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Recipe $recipe)
     {   
-        $recipe = DB::table('recipes')->find($id);
+     /*    $recipe = DB::table('recipes')->find($recipe); */
 
         return view('recipes.edit')->with('recipe', $recipe);
     }
