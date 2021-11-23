1b7113e44cdfc4908c3df07b2b5a59e9fcd8f9f0
abdelkader <gousmi.abdelkader@gmail.com>
2021-11-23 01:48:30 +0100

    changed delete image modal description in editimage page

--- resources/views/admin/recipes/editimage.blade.php
+++ resources/views/admin/recipes/editimage.blade.php
@@ -55,13 +55,13 @@
     @csrf    @method('delete')    <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Delete Recipe</h5>
+            <h5 class="modal-title" id="staticBackdropLabel">Delete Image</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            Are you sure you want the delete the selected recipe?
+            Are you sure you want the delete the selected Image?
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
